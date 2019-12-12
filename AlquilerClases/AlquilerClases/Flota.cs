a121ca9a8a061b466a6781e689a4b15c220285e7
renecabrera45 <renecabrera45.rc@gmail.com>
2019-12-12 19:10:32 -0300

    ClaseFlota

---
+++ AlquilerClases/AlquilerClases/Flota.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerClases
+{
+    public enum Estados{
+        Disponible,
+        Alquilado
+    }
+    class Flota
+    {
+        
+        public int Id { get; set; }
+        public Vehiculo vehiculo { get; set; }
+        public float precioDiario { get; set; }
+        public Estados estado { get; set; }
+        
+
+        public override string ToString()
+        {
+            return "Flota";
+        }
+
+        public static List<Flota> listadoFlota = new List<Flota>();
+
+        public static List<Flota> ObtenerFlota()
+        {
+            return listadoFlota;
+        }
+        public static void AgregarFlota(Flota f)
+        {
+            listadoFlota.Add(f);
+        }
+
+        public static void EliminarFlota(Flota f)
+        {
+            listadoFlota.Remove(f);
+        }
+
+        public static void EditarFlota(Flota f, int indice)
+        {
+
+            Flota.listadoFlota[indice] = f;
+        }
+    }
+}

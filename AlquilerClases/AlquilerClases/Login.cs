d0f81303a75b1066a78f4f205685070f5534563c
IbarraY2J <counterstrike16@hotmail.es>
2019-12-13 17:18:53 -0300

    login

---
+++ AlquilerClases/AlquilerClases/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerClases
+{
+    public class Log
+    {
+        private const string NombreArchivo = "log.txt";
+        private const string Separador = ";";
+        public static void EscribirLog(string TipoError, string Error)
+        {
+            using (StreamWriter w = File.AppendText(NombreArchivo))
+            {
+                string FechaHora = DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToLongTimeString();
+                string linea = FechaHora + Separador + TipoError + Separador + Error;
+                w.WriteLine(linea);
+
+            }
+
+
+        }
+
+
+    }
+}

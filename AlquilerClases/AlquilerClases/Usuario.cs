ce77a5d6f533d914495e930fe0771806099fc7e7
IbarraY2J <counterstrike16@hotmail.es>
2019-12-13 17:23:23 -0300

    usuario

---
+++ AlquilerClases/AlquilerClases/Usuario.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerClases
+{
+    public class Usuario
+    {
+        public static void CrearUsuario(string usuario, string password)
+        {
+            string password_protegido = EncodePassword(password);
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION)) 
+            {
+                
+                con.Open();
+
+                
+                string textoCmd = "INSERT INTO USUARIO (usuario, password) values(@Usuario, @password)";
+
+                
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+             
+                SqlParameter p1 = new SqlParameter("@Usuario", usuario.Trim());
+                p1.SqlDbType = SqlDbType.VarChar; 
+
+               
+                SqlParameter p2 = new SqlParameter("@Password", password_protegido);
+                p2.SqlDbType = SqlDbType.VarChar; 
+
+               
+                cmd.Parameters.Add(p1); 
+                cmd.Parameters.Add(p2); 
+
+                
+                try
+                {
+                    cmd.ExecuteNonQuery(); 
+                }
+                catch (SqlException ex)
+                {
+                    Log.EscribirLog("SqlException", ex.Message);
+                }
+
+
+
+
+            }
+        }
+
+        public static bool Autenticar(string usuario, string password)
+        {
+            string password_protegido = EncodePassword(password);
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                
+                con.Open();
+
+                
+                string textoCmd = "SELEC Usuario, password from Usuario where Usuario = @Usuario and password = @password";
+
+                
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+                
+                SqlParameter p1 = new SqlParameter("@Usuario", usuario.Trim());
+                p1.SqlDbType = SqlDbType.VarChar; 
+
+                
+                SqlParameter p2 = new SqlParameter("@Password", password_protegido);
+                p2.SqlDbType = SqlDbType.VarChar; 
+
+                
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2); 
+
+                try
+                {
+                    
+
+                    SqlDataReader reader = cmd.ExecuteReader(); 
+
+                    if (reader.HasRows) 
+                    {
+                        reader.Close(); 
+                        return true; 
+                    }
+                    else
+                    {
+                        reader.Close(); 
+                        return false; 
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Log.EscribirLog("SqlException", ex.Message);
+                    return false;
+                }
+
+
+
+            }
+        }
+
+
+        public static string EncodePassword(string originalPassword)
+        {
+
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            string salt = "0d71ee4472658cd5874c5578410a9d8611fc9aef";
+            string passwordSalt = salt + originalPassword;
+            byte[] inputBytes = (new UnicodeEncoding()).GetBytes(passwordSalt);
+            byte[] hash = sha1.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
